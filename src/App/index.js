c68d85a29bddec988c8098244a1abb61caab7d65
SahilAnejaxoxo <88663176+SahilAnejaxoxo@users.noreply.github.com>
2024-04-12 06:51:51 +0530

    add AppBar and Grid

--- src/App/index.js
+++ src/App/index.js
@@ -2,11 +2,13 @@ import './App.css';
 import WelcomeMessage from './welcome-message'
 import styled, {css} from 'styled-components'
 import AppLayout from './app-layout';
+import AppBar from './app-bar';
 
 function App() {
   // TODO TAHS
   return (
     <AppLayout>
+      <AppBar />
       <WelcomeMessage />
     </AppLayout>
   );
