1434cca6f1e88c31d35029e33dbb9b4a15a65b49
SahilAnejaxoxo <88663176+SahilAnejaxoxo@users.noreply.github.com>
2024-04-12 06:35:54 +0530

    Add styled Components

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,12 @@
 import './App.css';
+import WelcomeMessage from './welcome-message'
+import styled, {css} from 'styled-components'
 
 function App() {
   // TODO TAHS
   return (
     <div className="App">
+      <WelcomeMessage />
     </div>
   );
 }
